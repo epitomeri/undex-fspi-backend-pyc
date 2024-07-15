5bf631cb3e875f2917c183c939f319f13b9ee78d
Arya Buddha <40647186+AryaBuddha@users.noreply.github.com>
2024-07-15 14:19:48 -0400

    Fixed raw files1

--- app.py
+++ app.py
@@ -264,7 +264,9 @@ def handle_getgraphfiles(projectid):
     elif request.method == 'GET':
         project_base = f'./projects/{projectid}'
         displacement_graph_path = f'{project_base}/validation/blastfoam_displacement.png'
-        physiology_graph_path = f'{project_base}/validation/Physiology/pulseresults.csv'
+        physiology_graph_path = f'{os.getenv(
+            "PULSE_INSTALL_DIR"
+        )}/validation/Physiology/pulseresults.csv'
 
 
         graph_files = {
